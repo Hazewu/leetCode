1771dd56edded1a621cc8c574217ee58a8de4423
ersheng <wangzz@authine.com>
2021-01-07 20:11:32 +0800

    leetCode1
    Signed-off-by: ersheng <wangzz@authine.com>

---
+++ 1-10/sol1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int * twoSum(int* nums, int numsSize, int target){
+	int i, j;
+	int *result = NULL;			// int数组指针
+	for(i = 0;, i < numsSize-1; i++){
+		for(j = i + 1; j < numsSize; j++){
+			if(nums[i] + nums[j] == target){
+				result = (int*)malloc(sizeof(int)*2);			// sizeof(int)*2，表示申请两个int字节大的空间，一个int有4个字节，返回值为void型
+																// int*，表示将申请到的空间返回值void，强制转换为int*类型
+				result[0] = i;
+				result[1] = j;
+				return result;
+			}
+		}
+	}
+	return result;
+}
+
+int main(){
+	int nums[6] = {1, 2, 3, 4, 5, 6};
+	int numsSize = 6;
+	int target = 9;
+	int *result = twoSum(nums, numsSize, target);
+	printf("[%d, %d]\n", result[0], result[1]);
+    return 0;
+}
--- 1-10/sol1.cpp
+++ 1-10/sol1.cpp
@@ -3,4 +3,81 @@
 // 可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
 // 可以按任意顺序返回答案
 
-// 方法一：两层遍历，暴力解法
+#include<vector>
+#include<string>
+#include<iostream>
+#include<map>
+using namespace std;
+
+
+
+// // 方法一：两层遍历，暴力解法
+// class Solution{
+// public:
+// 	vector<int> twoSum(vector<int>& nums, int target){
+//         int i, j;
+//         for(i = 0; i < nums.size() - 1; i++){
+//         	for(j = i + 1; j < nums.size(); j++){
+//         		if(nums[i] + nums[j] == target){
+//         			return {i, j};						//中括号，才返回vector
+//         		}
+//         	}
+//         }
+//         return {i, j};
+// 	}
+// }
+
+// 时间复杂度O(n^2)
+// 空间复杂度O(1)
+// 运行速度慢，且内存空间消耗大
+
+
+// 方法二：两遍哈希表
+class Solution{
+	vector<int> twoSum(vector<int>& nums, int target){
+		map<int, int> a;				// 建立hash表存放数组元素
+		vector<int> result(2, -1)		// 存放结果，初始值为[-1, -1]
+        
+        // 存入字典，并且是索引作为value，可能会有问题，key可能会重复，这在map中是不允许的
+		for(int i = 0; i < nums.size(); i++){
+			a.insert(map<int, int>::value_type(nums[i], i));		// 字典类型？必须添加字典对象吗？可以a.insert(nums[i], i)吗
+		}
+
+		for(int i = 0; i < nums.size(); i++){
+            if(a.count(target - nums[i]) > 0 && (a[target-nums[i]] != i))		// a.count(target - nums[i]) > 0， 什么意思，count什么函数
+            																	// a[target-nums[i]] != i， 表示找到的这个数不能是nums[i]同一个数字
+		}
+	}
+}
+
+
+int main(){
+	cout << "hello, world" << endl;
+	return 0;
+}
+
+
+
+
+
+// vector知识点
+// 1、vector中的圆括号和花括号
+
+// int main{
+// 	vector<int> v_int{10};			// 花括号，一个数初始化（赋值）表示赋一个值，所以构造用花括号
+// 	vector<int> v_int2(10);			// 圆括号，一个数表示用几个相同元素初始化，
+
+// 	cout << "vector v_int:" << endl;
+// 	for(auto & i: v_int)			// 类似于迭代器？iter
+// 		cout << i << endl;
+//     cout << endl;
+
+//     cout << "vector v_int2:" << endl;
+//     for(auto & i : v_int2)
+//     	cout << i << endl;
+
+//     return 0;
+// }
+
+
+
